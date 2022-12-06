a5076c4bd392158e99931b63c723fbd9d9bf167b
Oscar Pinzon <oscar.eugenio.pinzon@gmail.com>
2022-12-06 15:57:08 -0500

    lint

--- models/Order.js
+++ models/Order.js
@@ -1,25 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const autoIncrementModelID = require('./Counter');
-
+const autoIncrementModelID = require("./Counter");
 
 //creates a Comment
 const OrderSchema = new Schema({
-    id: {type: Number ,
-    unique: true
-    },
-    userId:{type: Number,required: true},
-    dateTime:{type: String,required: true},
-    shippingAddress:{type: String,required: false},
+  id: { type: Number, unique: true },
+  userId: { type: Number, required: true },
+  dateTime: { type: String, required: true },
+  shippingAddress: { type: String, required: false },
 });
-OrderSchema.pre('save', function (next) {
-    if (!this.isNew) {
-      next();
-      return;
-    }
-  
-    autoIncrementModelID('order', this, next);
-  });
+OrderSchema.pre("save", function (next) {
+  if (!this.isNew) {
+    next();
+    return;
+  }
+
+  autoIncrementModelID("order", this, next);
+});
 
 //export
-module.exports = Comment = mongoose.model('order',OrderSchema);
+module.exports = Comment = mongoose.model("order", OrderSchema);
